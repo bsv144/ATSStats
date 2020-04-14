d97cf64ce39cb4d41b21817e895f82232f59f210
support <support@srv-docker-01.nrits.local>
2020-04-14 13:19:33 +0500

    добавил функция получения данных статистики

--- app/app/db.py
+++ app/app/db.py
@@ -5,7 +5,7 @@ from psycopg2.extensions import AsIs
 from psycopg2 import sql
 from dotenv import load_dotenv
 import pandas as pd
-
+import datetime
 
 #Загружаем переменные среды с файла
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -23,23 +23,6 @@ def get_ACDQueuesMembers():
 	con_Configuration = psycopg2.connect( dbname= DBNAME, user= USER, password= PASSWORD, host= HOST, port= PORT)
 	con_Security = psycopg2.connect( dbname= 'Cx_Security', user= USER, password= PASSWORD, host= HOST, port= PORT)
 
-	# Create a Cursor object that operates in the context of Connection con:
-	#cur = con.cursor(cursor_factory=RealDictCursor)
-	cur = con_Configuration.cursor()
-	cur_Security = con_Security.cursor()
-	# Execute the SELECT statement:
-	#cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
-	t1 = AsIs('public."C_ACDQueuesMembers"')
-	t2 = AsIs('public."U_Users"')
-	sql1 = "select * from %s"
-	cur.execute(sql1 , [t1])
-	cur_Security.execute(sql1 , [t2])
-	# Retrieve all rows as a sequence and print that sequence:
-	#print(cur.fetchall())
-	json_out = json.dumps(cur.fetchall())
-	##json_out = json.dumps(cur_Security.fetchall())
-	cur.close()
-	cur_Security.close()
 	sq_Users = pd.read_sql_query(
 		'''
 		select * from public."U_Users"
@@ -59,7 +42,7 @@ def get_ACDQueuesMembers():
 	con_Configuration.close()
 	con_Security.close()
 
-	df_Users 			= pd.DataFrame(sq_Users)
+	df_Users = pd.DataFrame(sq_Users)
 	df_ACDQueuesMembers = pd.DataFrame(sq_ACDQueuesMembers)
 	result = pd.merge(df_ACDQueuesMembers, df_Users, how='left', left_on='IDMember', right_on='ID')
 	#result_groups = result.groupby('IDACDQueue').apply(lambda x: x.to_json(orient='records'))
@@ -74,35 +57,69 @@ def get_ACDQueuesMembers():
 	json_out = {"CallGroups" : []}
 	for name, group in result_groups:
 		json_out["CallGroups"].append({"groupname" : name, "members" : group.to_json(orient='records')})
-	
 	json_out = json.dumps(json_out)
 	return json_out
 
-'''
-SELECT 
-  *
-FROM "Cx_Configuration".public."C_ACDQueuesMembers" as aqm
-JOIN "Cx_Configuration".public."C_ACDQueues" as aq on aqm."IDACDQueue" = aq."ID"
---JOIN ( SELECT * FROM dblink( 'dbname="Cx_Security"','select ID,Login,Name_F,Name_I,Name_O from public."U_Users"') as 
-JOIN ( SELECT * FROM dblink( 'select ID,Login,Name_F,Name_I,Name_O from "Cx_Security".public."U_Users"') as 
-uu(	"ID" bigint, 
-	"Login" character varying(100),
-	"Name_F" character varying(128),
-	"Name_I" character varying(64),
-	"Name_O" character varying(128)
+def get_StatisticsByCall():
+	con_Statistics = psycopg2.connect( dbname= 'Cx_Statistics', user= USER, password= PASSWORD, host= HOST, port= PORT)
+	odt = datetime.datetime(2020,4,14)
+	sq_ACDQueuesMembers = pd.read_sql_query(
+		'''
+WITH stats_seancescount AS  (
+	--Кол-во принятых звонков
+	select  "ANumberDialed",
+		sum("Общее кол-во звонков") as "Общее кол-во звонков",
+		sum("Обработано первым оператором") as "Обработано первым оператором",
+		sum("Обработано в IVR") as "Обработано в IVR",
+		sum("Обработано оператором") as "Обработано оператором",
+		sum("Потеряно во  время ожидания") as "Потеряно во  время ожидания"
+	from (
+		select "ANumberDialed",
+			count("ANumberDialed") as "Общее кол-во звонков",
+			case
+			   when "SeanceResult" = 101 then count("SeanceResult")
+			end as "Обработано первым оператором",
+			case
+			   when "SeanceResult" = 114 then count("SeanceResult")
+			end as "Обработано в IVR",
+			case
+			   when "SeanceResult" = 102 then count("SeanceResult")
+			end as "Обработано оператором",
+			case
+			   when "SeanceResult" = 191 then count("SeanceResult")
+			end as "Потеряно во  время ожидания"
+		from public."S_Seances"
+		WHERE "TimeStartDate" = {odt}
+		--Входящие звонки
+		and "SeanceType" = 1
+		GROUP BY "ANumberDialed","SeanceResult"
+	) as t1
+	GROUP BY "ANumberDialed"
+), stats_durationcalls as (
+	--Среднее время обработки звонка
+	select "ANumberDialed",
+		avg("DurationWait") as "Время ожидания", 
+		avg("DurationTalk") as "Время разговора"
+	from public."S_Seances"
+	WHERE "TimeStartDate" = {odt}
+		--Входящие звонки
+		and "SeanceType" = 1
+		--Данную статистику берём только для звонков обработанных первым опертором и оператором
+		and "SeanceResult" in (101,102)
+	GROUP BY "ANumberDialed"
 )
-) as uu on uu."ID" = aqm."IDMember"
-WHERE aq."Name" = 'Прием показаний'
- ;
 
+select *
+from stats_seancescount as ss
+left join stats_durationcalls as sd on ss."ANumberDialed" = sd."ANumberDialed"		
+		'''
+	, con_Statistics)
 
+	#Создаём словарь с данными для дальнейшего вывода в формате json
+        ##json_out = {"CallGroups" : []}
+        ##for name, group in result_groups:
+        ##        json_out["CallGroups"].append({"groupname" : name, "members" : group.to_json(orient='records')})
+	json_out = ""
 
-SELECT * FROM dblink( 'host=localhost port=10000 dbname="Cx_Security"','select ID,Login,Name_F,Name_I,Name_O from public."U_Users"') as 
-uu(	"ID" bigint, 
-	"Login" character varying(100),
-	"Name_F" character varying(128),
-	"Name_I" character varying(64),
-	"Name_O" character varying(128)
-)
-
-'''
+        json_out = json.dumps(json_out)
+	return json_out
